16241d7fa33a21e267c9253c706ceb11bf46755f
amir-awad <amirawad004@gmail.com>
2022-12-08 22:26:31 +0200

    :white_check_mark:made StringWithOneNumber test pass

--- src/main/java/com/mycompany/app/Calculator.java
+++ src/main/java/com/mycompany/app/Calculator.java
@@ -5,6 +5,9 @@ public class Calculator {
     public int evaluateString(String s) {
         if (s.equals(""))
             return 0;
+
+        if (s.length() == 1)
+            return Integer.parseInt(s);
         return -1;
     }
 
