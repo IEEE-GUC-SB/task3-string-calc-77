97a11c6ad36f54dd434282b111beed066fba108b
amir-awad <amirawad004@gmail.com>
2022-12-08 22:11:37 +0200

    :white_check_mark:made EmptyString test pass

---
+++ src/main/java/com/mycompany/app/Calculator.java
@@ -0,0 +1,11 @@
+package com.mycompany.app;
+
+public class Calculator {
+
+    public int evaluateString(String s) {
+        if (s.equals(""))
+            return 0;
+        return -1;
+    }
+
+}
--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -1,5 +1,6 @@
 package com.mycompany.app;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -7,14 +8,12 @@ import org.junit.Test;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+public class AppTest {
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void testEmptyString() {
+        Calculator calc = new Calculator();
+
+        assertEquals(0, calc.evaluateString(""));
     }
 }
