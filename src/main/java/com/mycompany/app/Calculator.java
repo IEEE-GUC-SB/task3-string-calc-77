b736a0316f3d26a44f172c764283750c71514952
amir-awad <amirawad004@gmail.com>
2022-12-09 02:10:41 +0200

    :recycle:refactoring Calculator.java

--- src/main/java/com/mycompany/app/Calculator.java
+++ src/main/java/com/mycompany/app/Calculator.java
@@ -1,18 +1,14 @@
 package com.mycompany.app;
 
-import java.lang.reflect.Array;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Calculator {
 
     public int evaluateString(String s) throws Exception {
         if (s.equals(""))
             return 0;
-
-        if (s.length() == 1)
-            return Integer.parseInt(s);
-        if (s.length() == 3)
-            return Integer.parseInt("" + s.charAt(0)) + Integer.parseInt("" + s.charAt(2));
 
         int sum = 0;
         ArrayList<String> delimiter = new ArrayList<>();
@@ -25,14 +21,14 @@ public class Calculator {
                 s = s.substring(4, s.length());
             } else if (s.charAt(2) == '[') {
                 delimiter.clear();
-                String currentDelimiter = "";
+                StringBuilder currentDelimiter = new StringBuilder();
                 int i = 3;
                 while (s.charAt(i) != '\n') {
                     if (s.charAt(i) != '[' && s.charAt(i) != ']') {
-                        currentDelimiter += s.charAt(i);
+                        currentDelimiter.append(s.charAt(i));
                     } else if (s.charAt(i) == ']') {
-                        delimiter.add(currentDelimiter);
-                        currentDelimiter = "";
+                        delimiter.add(currentDelimiter.toString());
+                        currentDelimiter.delete(0, currentDelimiter.length());
                     }
                     i++;
                 }
@@ -44,13 +40,12 @@ public class Calculator {
         }
 
         s = s.replace("\n", delimiter.get(0));
-        String[] nums = s.split("[" + delimiter + "]");
-        System.out.println(Arrays.toString(nums) + " nums");
-        HashSet<Integer> negatives = getNegatives(nums);
+        String[] numbers = s.split("[" + delimiter + "]");
+        Set<Integer> negatives = getNegatives(numbers);
         if (!negatives.isEmpty())
             throw new Exception("Negatives are not allowed. Negatives found are: " + negatives.toString());
 
-        for (String num : nums) {
+        for (String num : numbers) {
             if (num.length() > 0) {
                 int number = Integer.parseInt(num);
                 sum += number > 1000 ? 0 : number;
@@ -60,23 +55,17 @@ public class Calculator {
         return sum;
     }
 
-    public HashSet<Integer> getNegatives(String[] nums) {
-        HashSet<Integer> hs = new HashSet<>();
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i].length() > 0) {
-                int number = Integer.parseInt(nums[i]);
+    public Set<Integer> getNegatives(String[] numbers) {
+        Set<Integer> hs = new HashSet<>();
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i].length() > 0) {
+                int number = Integer.parseInt(numbers[i]);
                 if (number < 0)
                     hs.add(number);
             }
         }
 
         return hs;
-    }
-
-    public static void main(String[] args) throws Exception {
-        Calculator calc = new Calculator();
-        String s = "//[*][%]\n1*2%3";
-        System.out.println(calc.evaluateString(s));
     }
 
 }
